116133d90d5da5775f125df531ce3e78d531aa82
AhmedTrooper <151874789+AhmedTrooper@users.noreply.github.com>
2026-07-05 14:26:47 +0600

    style: Replace blurry RotateCw spinner with deterministic Loader2 ring in AI Co-Pilot

--- src/components/DiagramTab.vue
+++ src/components/DiagramTab.vue
@@ -1370,7 +1370,7 @@ const activeFileName = computed(() => {
                   @keyup.enter="refineWithAi"
                 />
                 <button @click="refineWithAi" :disabled="isRefining">
-                  <RotateCw v-if="isRefining" :size="14" class="spinner" />
+                  <Loader2 v-if="isRefining" :size="14" class="spinner" />
                   <span v-else>→</span>
                 </button>
               </Motion>
